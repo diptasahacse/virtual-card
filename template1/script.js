647c6d1a72c44beffb4e9fb0b4edbcfdd0b8eb8d
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-03-28 10:20:45 +0600

    -

---
+++ template1/script.js
@@ -0,0 +1,80 @@
+// Slick Services
+$(".all-services").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: false,
+    swipeToSlide: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    
+  });
+// Slick Gallery
+$(".all-gallery-items").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1300,
+    dots: false,
+    arrows: false,
+    swipeToSlide: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    
+  });
+// Slick Products
+$(".all-products-items").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1300,
+    dots: false,
+    arrows: false,
+    swipeToSlide: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    
+  });
+// Slick Blog
+$(".all-blog-items").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1300,
+    dots: false,
+    arrows: false,
+    swipeToSlide: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    
+  });
